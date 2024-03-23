1113db9f11a869af527b6318d02b7885dffaa274
AmirHossein Dashtban Namaghi <amirhdn6@gmail.com>
2024-03-23 10:05:33 +0330

    added ThemeModeContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { ThemeModeContext, modeSetting } from "./context/ThemeContext";
+import { ThemeProvider } from "@emotion/react";
 
 export default function App() {
-	return <div>App</div>;
+	const [toggleThemeMode, theme] = modeSetting();
+
+	return (
+		<ThemeModeContext.Provider value={toggleThemeMode}>
+			<ThemeProvider theme={theme}>App</ThemeProvider>
+		</ThemeModeContext.Provider>
+	);
 }
---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,81 @@
+import { createTheme } from "@mui/material/styles";
+import { createContext, useState, useMemo } from "react";
+
+const themeSetting = (mode) =>
+	mode === "dark"
+		? {
+				palette: {
+					action: {
+						active: "#33405C",
+					},
+					background: {
+						default: "#121C24",
+					},
+					border: {
+						primary: "#33405C",
+					},
+					button: {
+						primary: "#F5C754",
+						secondary: "#E3E8F2",
+						primaryText: "#141C24",
+						secondaryText: "#F7FAFA",
+					},
+					chart: {
+						primary: "#8A9EBF",
+					},
+					mode: mode,
+					text: {
+						primary: "#F7FAFA",
+						secondary: "#8A9EBF",
+					},
+				},
+		  }
+		: {
+				palette: {
+					action: {
+						active: "#E3E8F2",
+					},
+					background: {
+						default: "#F7FAFA",
+					},
+					border: {
+						primary: "#D4DBE8",
+					},
+					button: {
+						primary: "#F5C754",
+						secondary: "#E3E8F2",
+						primaryText: "#141C24",
+						secondaryText: "#141C24",
+					},
+					chart: {
+						primary: "#0F1622",
+					},
+					text: {
+						primary: "#141C24",
+						secondary: "#D4DBE8",
+					},
+				},
+		  };
+
+export const ThemeModeContext = createContext({
+	toggleThemeMode: () => {},
+});
+
+export const modeSetting = () => {
+	const [mode, setMode] = useState("dark");
+
+	const themeMode = useMemo(
+		() => ({
+			toggleThemeMode: () => {
+				setMode((prev) => (prev === "dark" ? "light" : "dark"));
+			},
+		}),
+		[]
+	);
+
+	const theme = useMemo(() => {
+		return createTheme(themeSetting(mode));
+	}, [mode]);
+
+	return [themeMode, theme];
+};
